8effd8f4be71ef4d25425b551d79b62a72611833
Srikanta Patanjali <pata@zhaw.ch>
2015-06-01 15:12:53 +0200

    v0.4.0

---
+++ src/main/java/ch/icclab/cyclops/resource/interfc/UsageResource.java
@@ -0,0 +1,19 @@
+package ch.icclab.cyclops.resource.interfc;
+
+import org.restlet.representation.Representation;
+
+import java.util.HashMap;
+
+/**
+ * Author: Srikanta
+ * Created on: 21-Jan-15
+ * Description:
+ * Change Log
+ * Name        Date     Comments
+ */
+public interface UsageResource extends UDRResource {
+
+    public Representation getData();
+    
+    public Representation constructResponse(HashMap usageArr, String userId, String fromDate, String toDate);
+}
