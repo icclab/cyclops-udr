2e39b85596cc4e4a36d8a309679e775b8e2a4454
Srikanta Patanjali <pata@zhaw.ch>
2015-07-30 16:46:27 +0200

    v0.4.1

--- install/install.sh
+++ install/install.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied, expected public IP of this node"
-    echo "Usage sudo ./install.sh [public IP]"
-    exit 1
-fi
-sudo ./package-installation.sh $1
+sudo ./package-installation.sh
 ./db-setup.sh
