7c84b97a4fc26f403b04dfcdf8f3c92c8e720440
Srikanta Patanjali <pata@zhaw.ch>
2015-08-11 10:19:32 +0200

    v0.5.2

--- install/db-setup.sh
+++ install/db-setup.sh
@@ -2,9 +2,11 @@
 echo "---------------------------------------------------------------------------"
 echo "| Setting up the database "
 echo "---------------------------------------------------------------------------"
-curl -X POST 'http://localhost:8086/db?u=root&p=root' -d '{"name": "udr_service"}'
-curl -X POST 'http://localhost:8086/db/udr_service/users?u=root&p=root' -d '{"name": "admin", "password": "Yh9hvmhGev"}'
-curl -X POST 'http://localhost:8086/db/udr_service/users?u=root&p=root' -d '{"name":"meterselection","columns":["time","source","metersource","metertype","metername","status"],"points":[[1427101641000,"cyclops-ui","openstack","gauge","cpu_util",1]]}'
+curl -X POST 'http://localhost:8086/cluster_admins?u=root&p=root' -d '{"name": "clusteradmin", "password": "changeit"}'
+curl -X POST 'http://localhost:8086/db?u=clusteradmin&p=changeit' -d '{"name": "udr_service"}'
+curl -X POST 'http://localhost:8086/db/udr_service/users?u=clusteradmin&p=changeit' -d '{"name": "dbadmin", "password": "changeit"}'
+curl -X POST 'http://localhost:8086/db/udr_service/users/dbadmin?u=clusteradmin&p=changeit' -d '{"admin": true}'
+curl -X POST 'http://localhost:8086/db/udr_service/users?u=dbadmin&p=changeit' -d '{"name":"meterselection","columns":["time","source","metersource","metertype","metername","status"],"points":[[1427101641000,"cyclops-ui","openstack","gauge","cpu_util",1]]}'
 echo "---------------------------------------------------------------------------"
 echo "| Installation process is complete "
 echo "---------------------------------------------------------------------------"
