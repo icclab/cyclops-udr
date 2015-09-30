956e77b7faa6ce81ef89290a4888bb72f5b41dcd
Manu Perez <perz@zhaw.ch>
2015-09-30 14:38:59 +0200

    Default InfluxDBClient.java

--- src/main/java/ch/icclab/cyclops/support/database/influxdb/client/InfluxDBClient.java
+++ src/main/java/ch/icclab/cyclops/support/database/influxdb/client/InfluxDBClient.java
@@ -20,200 +20,89 @@ package ch.icclab.cyclops.support.database.influxdb.client;
 import ch.icclab.cyclops.services.iaas.openstack.model.TSDBData;
 import ch.icclab.cyclops.util.Load;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.influxdb.InfluxDB;
-import org.influxdb.InfluxDBFactory;
-import org.influxdb.dto.Point;
-import org.influxdb.dto.Query;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.restlet.Client;
+import org.restlet.data.ChallengeScheme;
+import org.restlet.data.MediaType;
 import org.restlet.data.Protocol;
 import org.restlet.representation.Representation;
 import org.restlet.resource.ClientResource;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.TreeMap;
 
 /**
  * Author: Srikanta
  * Created on: 15-Oct-14
- * Upgraded by: Manu
- * Upgraded on: 23-Sep-15
  * Description: Client class for InfluxDB
- * <p/>
+ *
  * Change Log
  * Name        Date     Comments
  */
 public class InfluxDBClient extends ClientResource {
-    final static Logger logger = LogManager.getLogger(InfluxDBClient.class.getName());
-
     Load load = new Load();
     String url = load.configuration.get("InfluxDBURL");
     String username = load.configuration.get("InfluxDBUsername");
     String password = load.configuration.get("InfluxDBPassword");
-    String dbName = load.configuration.get("dbName");
 
     /**
      * Saves the data into InfluxDB via HTTP
-     * <p/>
+     *
      * Pseudo Code
      * 1. Load the login credentials from the configuration object
      * 2. Create a client instance and set the HTTP protocol, url and auth details
      * 3. Send the data
      *
-     * @param data - a JSON representation of the data
+     * @param data
      * @return boolean
      */
+    public boolean saveData(String data){
+        System.out.println("Entered the InfluxDBClient");
 
-    public boolean saveData(String data) {
-        InfluxDB influxDB = InfluxDBFactory.connect(load.configuration.get("InfluxDBURL"), "root", "root");
+        data = "["+data+"]";
         Representation output;
-        String[] columns = getColumns(data);
-        ArrayList<String[]> points = getPoints(data);
-        int meternameIndex = -1;
-        int timeIndex = -1;
-        int sourceIndex = -1;
-        int metersourceIndex = -1;
-        int metertypeIndex = -1;
-        int statusIndex = -1;
-        for (int i = 0; i < columns.length; i++) {
-            if (columns[i].equals("metername"))
-                meternameIndex = i;
-            else if (columns[i].equals("time"))
-                timeIndex = i;
-            else if (columns[i].equals("source"))
-                sourceIndex = i;
-            else if (columns[i].equals("metersource"))
-                metersourceIndex = i;
-            else if (columns[i].equals("metertype"))
-                metertypeIndex = i;
-            else if (columns[i].equals("status"))
-                statusIndex = i;
-        }
-        logger.debug("Obtained indexes from the columns: Metername: " + meternameIndex + " Time: " + timeIndex + " Source: " + sourceIndex + "Metersource: " + metersourceIndex + " Metertype: " + metersourceIndex + "Status: " + statusIndex);
-        for (int i = 0; i < points.size(); i++) {
-            logger.debug("Attempting to build the Point: " + points.get(i)[meternameIndex]);
-            Point point = Point.measurement(data.split("name\":\"")[1].split("\"")[0])
-                    .tag("time", points.get(i)[timeIndex])
-                    .tag("source", points.get(i)[sourceIndex].substring(1, points.get(i)[sourceIndex].length() - 1))
-                    .tag("metersource", points.get(i)[metersourceIndex].substring(1, points.get(i)[metersourceIndex].length() - 1))
-                    .tag("metertype", points.get(i)[metertypeIndex].substring(1, points.get(i)[metertypeIndex].length() - 1))
-                    .tag("metername", points.get(i)[meternameIndex].substring(1, points.get(i)[meternameIndex].length() - 1))
-                    .field("status", points.get(i)[statusIndex])
-                    .build();
-            logger.debug("Attempting to write the Point (" + points.get(i)[meternameIndex] + ") in the db:" + dbName);
-            influxDB.write(dbName, "default", point);
-            logger.debug("Point successfully written.");
-        }
+        System.out.println(data);
+
+        Client client = new Client(Protocol.HTTP);
+        ClientResource cr = new ClientResource(url);
+        cr.setChallengeResponse(ChallengeScheme.HTTP_BASIC, username, password);
+
+        cr.post(data);
+        output = cr.getResponseEntity();
+        System.out.println("Exit InfluxDBClient");
+
         return true;
     }
 
-    public TSDBData getData(String parameterQuery) {
-        logger.debug("Attempting to get Data");
-        InfluxDB influxDB = InfluxDBFactory.connect(load.configuration.get("InfluxDBURL"), "root", "root");
+    public TSDBData getData(String query){
         JSONArray resultArray;
         JSONObject resultObj;
-        TSDBData[] dataObj = null;
+        TSDBData dataObj = null;
         Representation output;
         ObjectMapper mapper = new ObjectMapper();
-        int timeIndex = -1;
-        int usageIndex = -1;
+
         Client client = new Client(Protocol.HTTP);
         ClientResource cr = new ClientResource(url);
-        Query query = new Query(parameterQuery, dbName);
+
+        cr.addQueryParameter("q",query);
+        cr.addQueryParameter("u",username);
+        cr.addQueryParameter("p",password);
+        cr.get(MediaType.APPLICATION_JSON);
+        output = cr.getResponseEntity();
+
         try {
-            logger.debug("Attempting to execute the query: " + parameterQuery + " into the db: " + dbName);
-            resultArray = new JSONArray(influxDB.query(query).getResults());
-            logger.debug("Obtained results: " + resultArray.toString());
-            if (!resultArray.isNull(0)) {
-                if (resultArray.toString().equals("[{}]")) {
-                    TSDBData data = new TSDBData();
-                    data.setColumns(new ArrayList<String>());
-                    data.setPoints(new ArrayList<ArrayList<Object>>());
-                    return data;
-                } else {
-                    JSONObject obj = (JSONObject) resultArray.get(0);
-                    JSONArray series = (JSONArray) obj.get("series");
-                    String response = series.get(0).toString();
-                    response = response.split("values")[0] + "points" + response.split("values")[1];
-                    series.put(0, new JSONObject(response));
-                    dataObj = mapper.readValue(series.toString(), TSDBData[].class);
-
-                    //Filter the points for repeated timestamps and add their usage/avg value
-                    for(int i = 0 ; i<dataObj.length ; i++){
-                        for(int o = 0 ; o<dataObj[0].getColumns().size(); o++) {
-                            if (dataObj[0].getColumns().get(o).equalsIgnoreCase("time"))
-                                timeIndex = o;
-                            if(dataObj[0].getColumns().get(o).equalsIgnoreCase("usage") || dataObj[0].getColumns().get(o).equalsIgnoreCase("avg"))
-                                usageIndex = o;
-                        }
-                        if(usageIndex>-1) {
-                            //If the json belongs to a meter point, filter and add to another if necessary.
-                            TreeMap<String, ArrayList> points = new TreeMap<String, ArrayList>();
-                            for (ArrayList point : dataObj[0].getPoints()) {
-                                if (points.containsKey(point.get(timeIndex))) {
-                                    String time = (String) point.get(timeIndex);
-                                    Double usage = Double.parseDouble(points.get(time).get(usageIndex).toString());
-                                    usage = Double.parseDouble(point.get(usageIndex).toString()) + usage;
-                                    point.set(usageIndex, usage);
-                                }
-                                points.put((String) point.get(timeIndex), point);
-                            }
-                            ArrayList<ArrayList<Object>> result = new ArrayList<ArrayList<Object>>();
-                            for (String key : points.keySet()) {
-                                result.add(points.get(key));
-                            }
-                            dataObj[0].setPoints(result);
-                        }
-                    }
-
-                }
+            resultArray = new JSONArray(output.getText());
+            if(!resultArray.isNull(0)){
+                resultObj = new JSONObject();
+                resultObj = (JSONObject) resultArray.get(0);
+                dataObj = mapper.readValue(resultObj.toString(),TSDBData.class);
             }
         } catch (JSONException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return dataObj[0];
-    }
-
-    private Long formatDate(String dateAndTime) {
-        Date result = null;
-        try {
-            String date = dateAndTime.split("T")[0];
-            String hour = dateAndTime.split("T")[1];
-            hour = hour.substring(0, 8);
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            result = formatter.parse(date + " " + hour);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return result.getTime();
-    }
-
-    private String[] getColumns(String json) {
-        String[] result = json.split(":\\[")[1].split("]")[0].split(",");
-        for (int i = 0; i < result.length; i++) {
-            result[i] = result[i].substring(1, result[i].length() - 1);
-        }
-        return result;
-    }
-
-    private ArrayList<String[]> getPoints(String json) {
-        ArrayList<String[]> result = new ArrayList<String[]>();
-        String[] split = json.split(":\\[")[2].split("],\\[");
-        split[0] = split[0].substring(1);
-        split[split.length - 1] = split[split.length - 1].substring(0,  split[split.length - 1].length() - 3);
-        for (int i = 0; i < split.length; i++) {
-            result.add(split[i].split(","));
-        }
-        return result;
+        return dataObj;
     }
 }
